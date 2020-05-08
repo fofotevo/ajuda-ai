2d4fc613cbe909e34a5cc24800dbf518646dbfed
ETomacheski <tomacheski2001@gmail.com>
2020-05-08 10:31:17 -0300

    adicionando campos no user

--- src/database/migrations/20200506150745-create-user.js
+++ src/database/migrations/20200506150745-create-user.js
@@ -1,14 +1,73 @@
 'use strict';
 
+const sequelize = require('sequelize');
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+      return queryInterface.createTable('users', { 
+        id: {
+          type: Sequelize.INTEGER ,
+          primaryKey:true,
+          autoIncrement:true,
+          allowNull:false,
+          },
+
+          name:{
+            type: Sequelize.STRING,
+            allowNull: false
+          },
+
+          address:{
+            type: Sequelize.STRING,
+            allowNull:false,
+          },
+          phone:{
+            type: Sequelize.STRING,
+          },
+          active:{
+            type: Sequelize.STRING,
+            
+          },
+
+          need:{
+            type: Sequelize.STRING,
+            allowNull:false,
+          },
+
+          membres_family:{
+            type: Sequelize.DECIMAL,
+            allowNull:false,
+          },
+          impact:{
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+
+          
+
+          income:{
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+
+          created_at:{
+            type: Sequelize.DATE,
+            defaultValue: sequelize.literal('NOW()'),
+            allowNull:false,
+          },
+
+          updated_at:{
+            type: Sequelize.DATE,
+            defaultValue: sequelize.literal('NOW()'),
+            allowNull:false,
+          },
+       });
     
   },
 
   down: (queryInterface, Sequelize) => {
-   
+
       return queryInterface.dropTable('users');
     
   }
