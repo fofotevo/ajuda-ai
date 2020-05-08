7dfc19a39e4ae0177fc7de2dc25cffe80fbc6822
Eduardo Dias <eduardodiasprog@gmail.com>
2020-05-08 14:08:43 -0300

    setting the server pattern

--- src/server.js
+++ src/server.js
@@ -1,3 +1,39 @@
+/* 
+*   Module dependecies.
+*/
+
+const http = require('http');
 const app = require('./app');
 
-app.listen(process.env.PORT || 3333);
+
+/*
+*   Get the port from environment and store in Express.
+*/
+
+const port = normalizePort(process.env.PORT || '3333');
+app.set('port', port);
+
+
+/*
+*   Create the HTTP server.
+*/
+
+const server = http.createServer(app);
+server.listen(port);
+
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
