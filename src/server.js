0c27425927b58b39996dfe5fd16f5fc8057bd889
Eduardo Dias <eduardodiasprog@gmail.com>
2020-05-08 14:37:40 -0300

    returning to a simple app server

--- src/server.js
+++ src/server.js
@@ -1,39 +1,4 @@
-/* 
-*   Module dependecies.
-*/
-
-// const http = require('http');
 const app = require('./app');
-
-
-/*
-*   Get the port from environment and store in Express.
-*/
-
 const port = (process.env.PORT || 3333);
-// app.set('port', port);
 
-
-/*
-*   Create the HTTP server.
-*/
-
-// const server = http.createServer(app);
 app.listen(port);
-
-
-// function normalizePort(val) {
-//     var port = parseInt(val, 10);
-
-//     if (isNaN(port)) {
-//         // named pipe
-//         return val;
-//     }
-
-//     if (port >= 0) {
-//         // port number
-//         return port;
-//     }
-
-//     return false;
-// }
