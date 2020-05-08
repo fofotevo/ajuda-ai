ddca226d75623ed0f72ae4ca52f2ae56248318cb
Eduardo Dias <eduardodiasprog@gmail.com>
2020-05-08 14:40:57 -0300

    returning to a complete http server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,39 @@
-const app = require('./app');
-const port = (process.env.PORT || 3333);
+/* 
+*   Module dependecies.
+*/
 
-app.listen(port);
+const http = require('http');
+const app = require('./app');
+
+
+/*
+*   Get the port from environment and store in Express.
+*/
+
+const port = normalizePort(process.env.PORT || '3333');
+app.set('port', port);
+
+
+/*
+*   Create the HTTP server.
+*/
+
+const server = http.createServer(app);
+server.listen(port);
+
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
