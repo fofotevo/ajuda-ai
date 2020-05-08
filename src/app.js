ef3d10b795e8f85a16733da377d256cafd4875f3
Eduardo Dias <eduardodiasprog@gmail.com>
2020-05-08 14:02:37 -0300

    adding the frontend configs to the app

--- src/app.js
+++ src/app.js
@@ -2,17 +2,20 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 require('./database/index');
-
 require('dotenv').config();
 
+// Frontend configs.
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // Parser configs.
-app.use(bodyParser.text({defaultCharset: 'utf-8'}));
+app.use(bodyParser.text({ defaultCharset: 'utf-8' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-const routes = require('./routes');
 
 app.use(routes);
 
