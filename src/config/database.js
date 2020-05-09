5817b5d10fff46ea089732bd4cc0331effd7d161
ETomacheski <tomacheski2001@gmail.com>
2020-05-09 00:01:02 -0300

    trocando banco de  dados para mysql

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,13 @@
 require('dotenv').config();
 module.exports = {
-    dialect: 'postgres',
+    dialect: 'mysql',
     dialectOptions: {
-        ssl: {
-          require: true,
-          // Ref.: https://github.com/brianc/node-postgres/issues/2009
-          rejectUnauthorized: false,
-        },
+        
         keepAlive: true,        
       },      
       ssl: true,
       host: process.env.DB_HOST,
-      port:5432,
+      port:3306,
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
