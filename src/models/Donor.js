440cb4225c32f5b65ccc60a6362852316b718348
ETomacheski <tomacheski2001@gmail.com>
2020-05-08 22:56:11 -0300

    model donor

---
+++ src/models/Donor.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require ('sequelize');
+
+class User extends Model {
+    
+    static init(sequelize){
+        
+        super.init({
+            name: DataTypes.STRING,
+            email: DataTypes.STRING,
+            phone: DataTypes.STRING,
+            
+        }, 
+        {
+            sequelize
+            
+        });
+    }
+}
+
+module.exports = User;
